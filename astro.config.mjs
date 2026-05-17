bca8daaf34c298cf349f865bccd157a546d6b8b0
tomipeti03 <tomanpepe03@gmail.com>
2026-05-17 20:02:25 +0200

    fixing config

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,15 +19,15 @@ export default defineConfig({
         social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
         sidebar: [
             {
-                label: 'Guides',
+                label: 'Overview',
                 items: [
                     // Each item here is one entry in the navigation menu.
-                    { label: 'Example Guide', slug: 'guides/example' },
+                    { label: 'Overview', slug: 'overview/overview' },
                 ],
             },
             {
-                label: 'Reference',
-                autogenerate: { directory: 'reference' },
+                label: 'Architectural Style',
+                autogenerate: { directory: 'as' },
             },
             {
                 label: 'ADRs',
